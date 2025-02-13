1c3abd6e4e1e6203334414076e0b3affecf42b9f
kim <yellsdesign@naver.com>
2025-02-13 09:46:17 +0900

    change

--- client/src/pages/NewProduct.jsx
+++ client/src/pages/NewProduct.jsx
@@ -1,26 +1,57 @@
-import React,{useState} from 'react';
+import React,{useRef, useState} from 'react';
 import '../scss/new_product.scss';
 import ImageUpload from '../components/ImageUpload.jsx';
 import axios from 'axios';
 
 export default function NewProduct() {
+    const refs = {
+        brendRef:useRef(null),
+        subjectRef:useRef(null),
+        descriptionRef:useRef(null),
+        priceRef:useRef(null),
+        dcRef:useRef(null)
+    };
+    const initForm = {
+        brend:'',
+        subject:'',
+        description:'',
+        price:'',
+        dc:''
+    }
+    let [formData, setFormData] = useState(initForm);
     const [fname, setFname] = useState({});
-    const [previewImg, setPreviewImg] = useState({});
-    const [formData, setFormData] = useState('');
-    const [currentImg, setCurrentImg] = useState('');
+    const [previewImg, setPreviewImg] = useState('');
+
 
     const getFileName = (filenames) => {
         setFname(filenames);
         setPreviewImg(`http://localhost:9000/${filenames.uploadFileName}`);
-        setCurrentImg(previewImg.replace(/\\/g, '/'));
-        // setCurrentImg(previewImg);
-        setFormData(filenames);
     }
-    console.log('previewImg',previewImg); // previewImg upload_files\1739284677715-KakaoTalk_20240213_211724594_02.jpg
-    
+    // input formdata
+    const changeFormData = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name] : value})
+    }
+    //validator
+    const validator = () => {
+        const newRef = Object.entries(refs);
+        for(const item of newRef){
+            const ref = item[1];
+            if(ref.current.value==='') {
+                ref.current.focus();
+                return false;
+            }
+            
+        }
+        return true;
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
-
+        
+        formData ={...formData,"uploadImg":fname.uploadFileName,"orgImg":fname.sourceFileName };
+        if(validator() )console.log('formData',formData);
+        
         axios.post('http://localhost:9000/product/new',formData)
                 .then(res => console.log(res.data))
                 .catch(err => console.log(err));
@@ -32,22 +63,34 @@ export default function NewProduct() {
             <h2>상품입력</h2>
             <form onSubmit={handleSubmit}>
                 <div className="form_area">
-                    <div className="f_wrap">
-                        <span>상품이름</span>
+                <div className="f_wrap">
+                        <span>브랜드명</span>
                         <div>
-                            <input type="text" name="id" placeholder='' />
+                            <input type="text" name="brend" ref={refs.brendRef} onChange={changeFormData} placeholder='' />
+                        </div>
+                    </div>
+                    <div className="f_wrap">
+                        <span>상품제목</span>
+                        <div>
+                            <input type="text" name="subject" ref={refs.subjectRef} onChange={changeFormData} placeholder='' />
+                        </div>
+                    </div>
+                    <div className="f_wrap">
+                        <span>상품 설명</span>
+                        <div>
+                            <input type="text" name="description" ref={refs.descriptionRef} onChange={changeFormData} placeholder='' />
                         </div>
                     </div>
                     <div className="f_wrap">
                         <span>가격</span>
                         <div>
-                            <input type="text" name="id" placeholder='' />
+                            <input type="text" name="price" ref={refs.priceRef} onChange={changeFormData} placeholder='' />
                         </div>
                     </div>
                     <div className="f_wrap">
-                        <span>상품이름</span>
+                        <span>할인율</span>
                         <div>
-                            <input type="text" name="id" placeholder='' />
+                            <input type="text" name="dc" ref={refs.dcRef} onChange={changeFormData} placeholder='' />
                         </div>
                     </div>
                     <div className="f_wrap">
@@ -78,7 +121,7 @@ export default function NewProduct() {
                                 <input type="text" name="source" value={fname.sourceFileName} readOnly />
                             </div>
                         </div>
-                        <div className='img'><img src={currentImg} alt="" /></div>
+                        <div className='img'><img src={previewImg} alt="" /></div>
                     </div>
                 </div>
 
